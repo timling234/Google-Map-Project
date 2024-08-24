cea26dc1417549be0bede95e59fd89e6f54645d4
timling234 <149541718+timling234@users.noreply.github.com>
2024-08-23 23:42:16 -0700

    Add files via upload

---
+++ work/main/src/m2.h
@@ -0,0 +1,28 @@
+/* 
+ * Copyright 2024 University of Toronto
+ *
+ * Permission is hereby granted, to use this software and associated 
+ * documentation files (the "Software") in course work at the University 
+ * of Toronto, or for personal use. Other uses are prohibited, in 
+ * particular the distribution of the Software either publicly or to third 
+ * parties.
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#pragma once
+
+// Draws the currently loaded map.
+// drawMap () assumes the map has already been loaded with loadMap ()
+// before it is called. The ece297exercise unit tests always call 
+// loadMap (string map_name) before calling drawMap. Your main () program
+// should follow the same convention.
+void drawMap();
